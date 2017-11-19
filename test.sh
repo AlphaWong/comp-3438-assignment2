e62bfeb6512af6d3ecea01dc706ceb10aa739097
alpha.wong <alpha.wong@lalamove.com>
2017-11-19 17:37:50 +0800

    ADD: Test.sh

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Building ..."
+gcc -o assignment2.out ./assignment2.c
+echo
+
+echo "Testing ..."
+./assignment2.out ./test.txt
+echo
+
+echo "Removeing the binary"
+rm ./assignment2.out
+echo
+
+echo "Completed"
+
