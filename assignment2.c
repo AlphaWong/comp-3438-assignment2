56ccd86c9867f0e886de8d29aa85fb140e8bfff6
alpha.wong <alpha.wong@lalamove.com>
2017-11-20 16:54:55 +0800

    FIX: String array length issue.

--- assignment2.c
+++ assignment2.c
@@ -4,19 +4,30 @@
 // Credit <alpha.wong@tuta.io>
 
 #include <fcntl.h>
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
 // Some definitions
-#define MAXBUFSIZE  1000
+#define MAXBUFSIZE 1000
 #define Max_Token_Size 100
 
 typedef enum {
-    KEYWORD, COMMA, SEMICOLON, ASSIGN,
-    PERIOD, NUM, PLUS, MINUS,
-    MUL, DIV, LBRACE, RBRACE, ID, NON
+  KEYWORD,
+  COMMA,
+  SEMICOLON,
+  ASSIGN,
+  PERIOD,
+  NUM,
+  PLUS,
+  MINUS,
+  MUL,
+  DIV,
+  LBRACE,
+  RBRACE,
+  ID,
+  NON
 } TokenType;
 
 // The Global variables
@@ -38,30 +49,30 @@ char tokenValue[Max_Token_Size];
 const char END_OF_READ = '\0';
 
 bool isAlphabet(const char c) {
-    char temp = c;
-    if ((temp >= 'a' && temp <= 'z') || (temp >= 'A' && temp <= 'Z')) {
-        return true;
-    }
-    return false;
+  char temp = c;
+  if ((temp >= 'a' && temp <= 'z') || (temp >= 'A' && temp <= 'Z')) {
+    return true;
+  }
+  return false;
 }
 
 bool isNumber(const char c) {
-    char temp = c;
-    if (temp >= '0' && temp <= '9') {
-        return true;
-    }
-    return false;
+  char temp = c;
+  if (temp >= '0' && temp <= '9') {
+    return true;
+  }
+  return false;
 }
 
 bool isKeyword() {
-    const char keywords[3][5] = {"var", "begin", "end"};
-    // i should below the length of keywords
-    for (int i = 0; i < sizeof (keywords) - 1; ++i) {
-        if (strcmp(tokenValue, keywords[i]) == 0) {
-            return true;
-        }
+  const char *keywords[3] = {"var", "begin", "end"};
+  // i should below the length of keywords
+  for (int i = 0; i < 3; ++i) {
+    if (strcmp(tokenValue, keywords[i]) == 0) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 // print_token will print the token based on its token type
@@ -72,49 +83,49 @@ bool isKeyword() {
 // MUL, DIV, LBRACE, RBRACE, ID*, NON
 
 void printToken(TokenType token) {
-    switch (token) {
-        case KEYWORD:
-            printf("KEYWORD\t\t\t\t| %s\n", tokenValue);
-            break;
-        case COMMA:
-            printf("COMMA\t\t\t\t|\n");
-            break;
-        case SEMICOLON:
-            printf("SEMICOLON\t\t\t|\n");
-            break;
-        case ASSIGN:
-            printf("ASSIGN\t\t\t\t|\n");
-            break;
-        case PERIOD:
-            printf("PERIOD\t\t\t\t|\n");
-            break;
-        case NUM:
-            printf("NUM\t\t\t\t| %s\n", tokenValue);
-            break;
-        case PLUS:
-            printf("PLUS\t\t\t\t|\n");
-            break;
-        case MINUS:
-            printf("MINUS\t\t\t\t|\n");
-            break;
-        case MUL:
-            printf("MUL\t\t\t\t|\n");
-            break;
-        case DIV:
-            printf("DIV\t\t\t\t|\n");
-            break;
-        case LBRACE:
-            printf("LBRACE\t\t\t\t|\n");
-            break;
-        case RBRACE:
-            printf("RBRACE\t\t\t\t|\n");
-            break;
-        case ID:
-            printf("ID\t\t\t\t| %s\n", tokenValue);
-            break;
-        case NON:
-            break;
-    }
+  switch (token) {
+  case KEYWORD:
+    printf("KEYWORD\t\t\t\t| %s\n", tokenValue);
+    break;
+  case COMMA:
+    printf("COMMA\t\t\t\t|\n");
+    break;
+  case SEMICOLON:
+    printf("SEMICOLON\t\t\t|\n");
+    break;
+  case ASSIGN:
+    printf("ASSIGN\t\t\t\t|\n");
+    break;
+  case PERIOD:
+    printf("PERIOD\t\t\t\t|\n");
+    break;
+  case NUM:
+    printf("NUM\t\t\t\t| %s\n", tokenValue);
+    break;
+  case PLUS:
+    printf("PLUS\t\t\t\t|\n");
+    break;
+  case MINUS:
+    printf("MINUS\t\t\t\t|\n");
+    break;
+  case MUL:
+    printf("MUL\t\t\t\t|\n");
+    break;
+  case DIV:
+    printf("DIV\t\t\t\t|\n");
+    break;
+  case LBRACE:
+    printf("LBRACE\t\t\t\t|\n");
+    break;
+  case RBRACE:
+    printf("RBRACE\t\t\t\t|\n");
+    break;
+  case ID:
+    printf("ID\t\t\t\t| %s\n", tokenValue);
+    break;
+  case NON:
+    break;
+  }
 }
 
 // getNextSyntax will move the token value
@@ -122,13 +133,13 @@ void printToken(TokenType token) {
 // @return int nf means next forword
 
 int getNextSyntax(const int f) {
-    int i;
-    for (i = 0; i < f; i++) {
-        tokenValue[i] = startPoint[i];
-    }
-    tokenValue[i] = END_OF_READ;
-    int nf = f - 1;
-    return nf;
+  int i;
+  for (i = 0; i < f; i++) {
+    tokenValue[i] = startPoint[i];
+  }
+  tokenValue[i] = END_OF_READ;
+  int nf = f - 1;
+  return nf;
 }
 
 // get_next_token will iterate the char
@@ -137,172 +148,166 @@ int getNextSyntax(const int f) {
 // @return -2 if the char is illegal
 
 int getNextToken() {
-    char c;
-    int currentState = 0;
-    forward = 0;
+  char c;
+  int currentState = 0;
+  forward = 0;
 
-    while (1) {
-        c = startPoint[forward];
-        switch (currentState) {
-            case 0:
-                if (isAlphabet(c)) {
-                    forward++;
-                    currentState = 1;
-                } else if (isNumber(c)) {
-                    forward++;
-                    currentState = 3;
-                } else {
-                    currentState = 7;
-                }
-                break;
+  while (1) {
+    c = startPoint[forward];
+    switch (currentState) {
+    case 0:
+      if (isAlphabet(c)) {
+        forward++;
+        currentState = 1;
+      } else if (isNumber(c)) {
+        forward++;
+        currentState = 3;
+      } else {
+        currentState = 7;
+      }
+      break;
 
-            case 1:
-                if (isAlphabet(c) || isNumber(c)) {
-                    forward++;
-                    currentState = 1;
-                } else {
-                    currentState = 2;
-                }
-                break;
+    case 1:
+      if (isAlphabet(c) || isNumber(c)) {
+        forward++;
+        currentState = 1;
+      } else {
+        currentState = 2;
+      }
+      break;
 
-            case 2:
-                forward = getNextSyntax(forward);
-                return isKeyword() ? KEYWORD : ID;
-                break;
+    case 2:
+      forward = getNextSyntax(forward);
+      return isKeyword() ? KEYWORD : ID;
+      break;
 
-            case 3:
-                if (c == '.') {
-                    forward++;
-                    currentState = 4;
-                } else if (isNumber(c)) {
-                    forward++;
-                    currentState = 3;
-                } else {
-                    forward = getNextSyntax(forward);
-                    return NUM;
-                }
-                break;
+    case 3:
+      if (c == '.') {
+        forward++;
+        currentState = 4;
+      } else if (isNumber(c)) {
+        forward++;
+        currentState = 3;
+      } else {
+        forward = getNextSyntax(forward);
+        return NUM;
+      }
+      break;
 
-            case 4:
-            case 5:
-                if (isNumber(c)) {
-                    forward++;
-                    currentState = 4;
-                } else {
-                    forward = getNextSyntax(forward);
-                    return NUM;
-                }
-                break;
+    case 4:
+    case 5:
+      if (isNumber(c)) {
+        forward++;
+        currentState = 4;
+      } else {
+        forward = getNextSyntax(forward);
+        return NUM;
+      }
+      break;
 
-            case 7:
-                if (
-                        c == ' '
-                        || c == '\t'
-                        || c == '\n'
-                        || c == '\v'
-                        || c == '\f'
-                        || c == '\r'
-                        ) {
-                    return NON;
-                }
+    case 7:
+      if (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' ||
+          c == '\r') {
+        return NON;
+      }
 
-                if (c == ',') {
-                    currentState = 8;
-                    break;
-                }
+      if (c == ',') {
+        currentState = 8;
+        break;
+      }
 
-                if (c == ';') {
-                    currentState = 9;
-                    break;
-                }
+      if (c == ';') {
+        currentState = 9;
+        break;
+      }
 
-                if (c == '=') {
-                    currentState = 10;
-                    break;
-                }
+      if (c == '=') {
+        currentState = 10;
+        break;
+      }
 
-                if (c == '.') {
-                    currentState = 11;
-                    break;
-                }
+      if (c == '.') {
+        currentState = 11;
+        break;
+      }
 
-                if (c == '+') {
-                    currentState = 12;
-                    break;
-                }
+      if (c == '+') {
+        currentState = 12;
+        break;
+      }
 
-                if (c == '-') {
-                    currentState = 13;
-                    break;
-                }
+      if (c == '-') {
+        currentState = 13;
+        break;
+      }
 
-                if (c == '*') {
-                    currentState = 14;
-                    break;
-                }
+      if (c == '*') {
+        currentState = 14;
+        break;
+      }
 
-                if (c == '/') {
-                    currentState = 15;
-                    break;
-                }
+      if (c == '/') {
+        currentState = 15;
+        break;
+      }
 
-                if (c == '(') {
-                    currentState = 16;
-                    break;
-                }
+      if (c == '(') {
+        currentState = 16;
+        break;
+      }
 
-                if (c == ')') {
-                    currentState = 17;
-                    break;
-                }
+      if (c == ')') {
+        currentState = 17;
+        break;
+      }
 
-                if (c == END_OF_READ) {
-                    currentState = 18;
-                    break;
-                } else {
-                    currentState = 19;
-                }
-                break;
+      if (c == END_OF_READ) {
+        currentState = 18;
+        break;
+      } else {
+        currentState = 19;
+      }
+      break;
 
-            case 8:
-                return COMMA;
-            case 9:
-                return SEMICOLON;
-            case 10:
-                return ASSIGN;
-            case 11:
-                return PERIOD;
-            case 12:
-                return PLUS;
-            case 13:
-                return MINUS;
-            case 14:
-                return MUL;
-            case 15:
-                return DIV;
-            case 16:
-                return LBRACE;
-            case 17:
-                return RBRACE;
-            case 18:
-                return -1;
-            case 19:
-                return -2;
-            default:
-                printf("Wrong state (should not occur)\n");
-                return -1;
-        }
+    case 8:
+      return COMMA;
+    case 9:
+      return SEMICOLON;
+    case 10:
+      return ASSIGN;
+    case 11:
+      return PERIOD;
+    case 12:
+      return PLUS;
+    case 13:
+      return MINUS;
+    case 14:
+      return MUL;
+    case 15:
+      return DIV;
+    case 16:
+      return LBRACE;
+    case 17:
+      return RBRACE;
+    case 18:
+      return -1;
+    case 19:
+      return -2;
+    default:
+      printf("Wrong state (should not occur)\n");
+      return -1;
     }
+  }
 }
 
 int openFile(const char *filePath) {
-    int fd;
-    // Open the source file
-    if ((fd = open(filePath, O_RDONLY)) < 0) {
-        printf("Can not open the file (File_Name:%s).\n", filePath);
-        return false;
-    }
-    return fd;
+  int fd;
+  // Open the source file
+  if ((fd = open(filePath, O_RDONLY)) < 0) {
+    printf("Can not open the file (File_Name:%s).\n", filePath);
+    return false;
+  }
+  return fd;
 }
 
 // parseFile will read the file
@@ -310,51 +315,53 @@ int openFile(const char *filePath) {
 // @return -1 if file cannot read
 
 bool parseFile(const char *arg) {
-    int fd, tokenName, rbytes;
+  int fd, tokenName, rbytes;
 
-    fd = openFile(arg);
-    // Maximum buffer size
-    char inputBuffer[MAXBUFSIZE];
+  fd = openFile(arg);
+  // Maximum buffer size
+  char inputBuffer[MAXBUFSIZE];
 
-    startPoint = inputBuffer;
+  startPoint = inputBuffer;
 
-    // Read the input file into a buffer
-    // Inputed file cannot bigger than MAXBUFSIZE
-    rbytes = read(fd, inputBuffer, MAXBUFSIZE);
+  // Read the input file into a buffer
+  // Inputed file cannot bigger than MAXBUFSIZE
+  rbytes = read(fd, inputBuffer, MAXBUFSIZE);
 
-    if (rbytes < 0) {
-        printf("Could not read from the file\n");
-        close(fd);
-        return false;
-    } else if (rbytes == 0) {
-        printf("The input file is empty.\n");
-        close(fd);
-        return false;
-    } else {
-        inputBuffer[rbytes - 1] = END_OF_READ;
-    }
-
-    printf("Token Name\t\t\t|Token Value \n");
-    printf("--------------------------------------------\n");
-    while ((tokenName = getNextToken()) >= 0) {
-        printToken(tokenName);
-        startPoint = startPoint + forward + 1;
-    }
-
-    if (tokenName == -1) printf("\nSuccessfully finish scan.\n");
-    else printf("\nError: Non-recognizable character in the input file.\n");
-
+  if (rbytes < 0) {
+    printf("Could not read from the file\n");
     close(fd);
     return false;
+  } else if (rbytes == 0) {
+    printf("The input file is empty.\n");
+    close(fd);
+    return false;
+  } else {
+    inputBuffer[rbytes - 1] = END_OF_READ;
+  }
+
+  printf("Token Name\t\t\t|Token Value \n");
+  printf("--------------------------------------------\n");
+  while ((tokenName = getNextToken()) >= 0) {
+    printToken(tokenName);
+    startPoint = startPoint + forward + 1;
+  }
+
+  if (tokenName == -1)
+    printf("\nSuccessfully finish scan.\n");
+  else
+    printf("\nError: Non-recognizable character in the input file.\n");
+
+  close(fd);
+  return false;
 }
 
 bool main(int argc, char *argv[]) {
-    // Check the number of input
-    if (argc != 2) {
-        printf("Usage: scan input_file \n");
-        return false;
-    }
+  // Check the number of input
+  if (argc != 2) {
+    printf("Usage: scan input_file \n");
+    return false;
+  }
 
-    // Process lexical analysis
-    return parseFile(argv[1]) > 0;
+  // Process lexical analysis
+  return parseFile(argv[1]) > 0;
 }
